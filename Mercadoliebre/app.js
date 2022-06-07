276186197cae69ae7442d14f63e387bfd7a1a2e6
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-06-07 02:21:03 -0300

    cambios

---
+++ Mercadoliebre/app.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+app.listen(3000, () => console.log('Corriendo el puerto 3000'));
